5a8262259480f475a0846d072a7d4ea72e5a2ed2
Wagner Trindade <wtrindades@hotmail.com>
2015-07-03 00:02:08 -0300

    More informations for perfis

--- project/core/models.py
+++ project/core/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 class Perfil(object):
 
-	def __init__(self, nome='', email=''):
+	def __init__(self, foto='', nome='', email='', dtNascimento='', localidade='', cargo='', empresa='', curso='', escola=''):
 		self.nome = nome
 		self.email = email
+		self.dtNascimento = dtNascimento
+		self.localidade = localidade
+		self.cargo = cargo
+		self.empresa = empresa
+		self.curso = curso
+		self.escola = escola
--- project/core/views.py
+++ project/core/views.py
@@ -8,6 +8,6 @@ def showPerfil(resquest, perfil_id):
 	perfil = Perfil()
 
 	if perfil_id == '1':
-		perfil = Perfil(nome="Steve Vai", email="steve@vai.com")
+		perfil = Perfil(nome='Steve Vai', email='steve@vai.com', dtNascimento='10/05/1978', localidade='EUA', cargo='Guitarrista', empresa='Steve Vai', curso='Arpegios', escola='Malmsteen School' )
 
 	return render(resquest, 'core/perfis.html', { 'perfil' : perfil })
