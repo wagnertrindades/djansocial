4e51341716941610f576e242cb272bb0f3a14e62
Wagner Trindade <wtrindades@hotmail.com>
2015-07-04 23:58:53 -0300

    Add perfis list in index and update front end index

--- project/core/views.py
+++ project/core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from project.perfis.models import Perfil
 
 def index(resquest):
-	return render(resquest, 'core/index.html')
+
+	allPerfis = Perfil.objects.all()
+	return render(resquest, 'core/index.html', { 'perfis' : allPerfis })
