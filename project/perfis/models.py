db573b9605a4c4ef6ecb3a08c4f18b75ba3e19e3
Wagner Trindade <wtrindades@hotmail.com>
2015-07-08 13:38:13 -0300

    Refatoring Perfis Models

--- project/perfis/models.py
+++ project/perfis/models.py
@@ -3,15 +3,20 @@ from django.utils import timezone
 
 class Perfil(models.Model):
 
-	nome = models.CharField(max_length=255, null=False)
-	email = models.CharField(max_length=255, null=False)
-	dtNascimento = models.CharField(max_length=10, null=False)
-	localidade = models.CharField(max_length=255, null=True)
-	cargo = models.CharField(max_length=155, null=True)
-	empresa = models.CharField(max_length=155, null=True)
-	curso = models.CharField(max_length=155, null=True)
-	escola = models.CharField(max_length=155, null=True)
-	dtCadastro = models.DateTimeField(editable=False, default=timezone.now) 
+	name = models.CharField('Nome', max_length=100)
+	slug = models.SlugField('Atalho')
+	email = models.CharField('E-mail', max_length=100)
+	dtNascimento = models.DateField('Data de Nascimento')
+	localidade = models.CharField('Localidade',max_length=255, blank=True)
+	cargo = models.CharField('Cargo', max_length=155, null=True)
+	empresa = models.CharField('Empresa', max_length=155, null=True)
+	curso = models.CharField('Curso', max_length=155, null=True)
+	escola = models.CharField('Escola', max_length=155, null=True)
+	dtCadastro = models.DateTimeField('Criado em', auto_now_add=True)
+	dtAtualizacao = models.DateTimeField('Atualizado em', auto_now=True) 
+
+	def __str__(self):
+		return self.name
 
 	def convidar(self, perfil_convidado):
 		Convite(solicitante = self, convidado = perfil_convidado).save()
